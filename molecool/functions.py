2fea717d36be16c730b02bf061ae06cae0edf5ad
yzchen-phy <yanzhucyz@gmail.com>
2022-07-15 13:47:16 -0400

    add open_pdb function to molecule

--- molecool/functions.py
+++ molecool/functions.py
@@ -62,17 +62,22 @@ def calculate_distance(rA, rB):
 
 def open_pdb(f_loc):
     # This function reads in a pdb file and returns the atom names and coordinates.
+
     with open(f_loc) as f:
         data = f.readlines()
-    c = []
-    sym = []
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
-    return sym, coords
+
+    coordinates = []
+    symbols = []
+    for line in data:
+        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
+            symbols.append(l[76:79].strip())
+            atom_coords = [float(x) for x in l[30:55].split()]
+            coordinates.append(atom_coords)
+
+    coords = np.array(coordinates)
+    symbols = np.array(symbols)
+
+    return symbols, coords
 
 atomic_weights = {
     'H': 1.00784,
@@ -99,7 +104,10 @@ def write_xyz(file_location, symbols, coordinates):
     
     # Write an xyz file given a file location, symbols, and coordinates.
     num_atoms = len(symbols)
-    
+
+    if num_atoms != len(coordinates):
+        raise ValueError(f"write_xyz : the number of symbols ({num_atoms}) and number of coordinates ({len(coordinates)}) must be the same to write xyz file!")
+   
     with open(file_location, 'w+') as f:
         f.write('{}\n'.format(num_atoms))
         f.write('XYZ file\n')
